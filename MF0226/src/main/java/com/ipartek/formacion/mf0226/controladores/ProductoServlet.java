d7afa06a9ae0e32f5d25cd37fbac01e5a99253ec
Javier Lete <javier@eskura21.com>
2023-05-08 09:31:41 +0200

    Añadir y modificar

--- MF0226/src/main/java/com/ipartek/formacion/mf0226/controladores/ProductoServlet.java
+++ MF0226/src/main/java/com/ipartek/formacion/mf0226/controladores/ProductoServlet.java
@@ -1,7 +1,11 @@
 package com.ipartek.formacion.mf0226.controladores;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import com.ipartek.formacion.mf0226.logicanegocio.AdministradorNegocio;
+import com.ipartek.formacion.mf0226.logicanegocio.AdministradorNegocioImpl;
 import com.ipartek.formacion.mf0226.logicanegocio.UsuarioNegocio;
 import com.ipartek.formacion.mf0226.logicanegocio.UsuarioNegocioImpl;
 import com.ipartek.formacion.mf0226.modelos.Producto;
@@ -15,25 +19,50 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/producto")
 public class ProductoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	private UsuarioNegocio negocio = new UsuarioNegocioImpl();
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private AdministradorNegocio adminNegocio = new AdministradorNegocioImpl();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String parId = request.getParameter("id");
-		
-		if(parId != null) {
+
+		if (parId != null) {
 			Long id = Long.parseLong(parId);
-			
+
 			Producto producto = negocio.detalle(id);
-			
+
 			request.setAttribute("producto", producto);
 		}
-		
+
 		request.getRequestDispatcher("/WEB-INF/vistas/producto.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String parId = request.getParameter("id");
+		String nombre = request.getParameter("nombre");
+		String parPrecio = request.getParameter("precio");
+		String parGarantia = request.getParameter("garantia");
+
+		Long id = null;
+
+		if (parId != null && !parId.isBlank()) {
+			id = Long.parseLong(parId);
+		}
+		
+		BigDecimal precio = new BigDecimal(parPrecio);
+		LocalDate garantia = LocalDate.parse(parGarantia);
+
+		Producto producto = Producto.builder().id(id).nombre(nombre).precio(precio).garantia(garantia).build();
+
+		if(producto.getId() == null) {
+			adminNegocio.agregarProducto(producto);
+		} else {
+			adminNegocio.modificarProducto(producto);
+		}
+		
+		response.sendRedirect("index");
 	}
 
 }
--- MF0226/src/main/webapp/WEB-INF/vistas/producto.jsp
+++ MF0226/src/main/webapp/WEB-INF/vistas/producto.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,7 +26,12 @@
 			<input type="date" id="garantia" name="garantia" value="${producto.garantia}">
 		</div>
 		<div>
-			<button>Guardar</button>
+			<c:if test="${producto.id == null}">
+				<button>Añadir</button>
+			</c:if>
+			<c:if test="${producto.id != null}">
+				<button>Modificar</button>
+			</c:if>
 		</div>
 	</form>
 </body>
