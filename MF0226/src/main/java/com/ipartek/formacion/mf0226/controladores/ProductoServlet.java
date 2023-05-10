c037e3ccaa8e7c08993d27b4e8a035cf71f6bdd8
Javier Lete <javier@eskura21.com>
2023-05-10 10:04:05 +0200

    Invocar la validación y convertir el resultado de validaciones en mapa

--- MF0226/src/main/java/com/ipartek/formacion/mf0226/controladores/ProductoServlet.java
+++ MF0226/src/main/java/com/ipartek/formacion/mf0226/controladores/ProductoServlet.java
@@ -3,6 +3,9 @@ package com.ipartek.formacion.mf0226.controladores;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import com.ipartek.formacion.mf0226.logicanegocio.AdministradorNegocio;
 import com.ipartek.formacion.mf0226.logicanegocio.AdministradorNegocioImpl;
@@ -15,7 +18,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import lombok.extern.java.Log;
 
+@Log
 @WebServlet("/producto")
 public class ProductoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -23,6 +32,9 @@ public class ProductoServlet extends HttpServlet {
 	private UsuarioNegocio negocio = new UsuarioNegocioImpl();
 	private AdministradorNegocio adminNegocio = new AdministradorNegocioImpl();
 
+	private ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private Validator validator = factory.getValidator();
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String parId = request.getParameter("id");
@@ -51,7 +63,7 @@ public class ProductoServlet extends HttpServlet {
 			id = Long.parseLong(parId);
 		}
 		
-		BigDecimal precio = new BigDecimal(parPrecio);
+		BigDecimal precio = parPrecio != null && !parPrecio.isBlank() ? new BigDecimal(parPrecio) : null;
 		LocalDate garantia = null;
 		
 		if(parGarantia != null && !parGarantia.isBlank()) {
@@ -60,6 +72,32 @@ public class ProductoServlet extends HttpServlet {
 
 		Producto producto = Producto.builder().id(id).nombre(nombre).precio(precio).garantia(garantia).build();
 
+		Set<ConstraintViolation<Producto>> validaciones = validator.validate(producto);
+		
+		Map<String, String> errores = new HashMap<>();
+		
+		// validaciones.forEach(validacion -> errores.put(validacion.getPropertyPath().toString(), validacion.getMessage()));
+		
+		String clave;
+		String valor;
+		
+		for(ConstraintViolation<Producto> validacion: validaciones) {
+			clave = validacion.getPropertyPath().toString();
+			valor = validacion.getMessage();
+			
+			errores.put(clave, valor);
+		}
+		
+		log.info(errores.toString());
+		
+		if(errores.size() > 0) {
+			request.setAttribute("errores", errores);
+			request.setAttribute("producto", producto);
+			request.getRequestDispatcher("/WEB-INF/vistas/producto.jsp").forward(request, response);
+			
+			return;
+		}
+		
 		if(producto.getId() == null) {
 			adminNegocio.agregarProducto(producto);
 		} else {
