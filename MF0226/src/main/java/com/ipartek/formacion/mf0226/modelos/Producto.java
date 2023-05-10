b9e69bde7d911742ab09c165c4e9b45eee4f85ec
Javier Lete <javier@eskura21.com>
2023-05-10 11:06:04 +0200

    Anotamos la clase con JPA

--- MF0226/src/main/java/com/ipartek/formacion/mf0226/modelos/Producto.java
+++ MF0226/src/main/java/com/ipartek/formacion/mf0226/modelos/Producto.java
@@ -3,6 +3,11 @@ package com.ipartek.formacion.mf0226.modelos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -16,18 +21,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Min(0)
 	private Long id;
-	
+
 	@NotNull
 	@Size(min = 3, max = 50)
 	private String nombre;
-	
+
 	@NotNull(message = "no debe estar vacío")
 	@Min(0)
 	private BigDecimal precio;
-	
+
 	@Future
 	private LocalDate garantia;
 }
