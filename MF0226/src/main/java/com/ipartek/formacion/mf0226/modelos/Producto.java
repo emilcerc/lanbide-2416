9b52056319d1d21510fc04b45c39c77a318c21f3
Javier Lete <javier@eskura21.com>
2023-05-10 10:03:10 +0200

    Definimos en la clase las restricciones que queremos que tenga

--- MF0226/src/main/java/com/ipartek/formacion/mf0226/modelos/Producto.java
+++ MF0226/src/main/java/com/ipartek/formacion/mf0226/modelos/Producto.java
@@ -3,6 +3,10 @@ package com.ipartek.formacion.mf0226.modelos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +17,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Producto {
+	@Min(0)
 	private Long id;
+	
+	@NotNull
+	@Size(min = 3, max = 50)
 	private String nombre;
+	
+	@NotNull(message = "no debe estar vacío")
+	@Min(0)
 	private BigDecimal precio;
+	
+	@Future
 	private LocalDate garantia;
 }
