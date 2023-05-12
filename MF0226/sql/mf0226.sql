1459ee8fbd898235d4e0b0ad6a1c630db2a43a1c
Javier Lete <javier@eskura21.com>
2023-05-12 08:18:55 +0200

    Backup de la base de datos con productos, usuarios y roles

--- MF0226/sql/mf0226.sql
+++ MF0226/sql/mf0226.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `mf0226` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `mf0226`;
+CREATE DATABASE  IF NOT EXISTS `mf0226jpa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mf0226jpa`;
 -- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
--- Host: localhost    Database: mf0226
+-- Host: 127.0.0.1    Database: mf0226jpa
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
@@ -26,11 +26,11 @@ DROP TABLE IF EXISTS `productos`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `productos` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
-  `precio` decimal(19,2) NOT NULL,
   `garantia` date DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio` decimal(38,2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,16 +39,71 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Producto1',1234.00,'2023-05-18'),(2,'Producto2',234.00,NULL),(4,'asdfasdf',1234.00,'2023-05-18'),(5,'asdf',433.00,'2023-05-10'),(6,'asdfas',1234.00,NULL);
+INSERT INTO `productos` VALUES (1,NULL,'Producto1',1234.00),(2,'2024-01-01','Producto2',2345.00),(3,NULL,'Producto3',3456.00),(4,'2024-01-01','Producto4',4567.00),(5,NULL,'Producto5',5678.00),(6,'2024-01-01','Producto6',6789.00),(7,NULL,'Producto7',7890.00);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Dumping events for database 'mf0226'
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `descripcion` tinytext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ADMIN','Administradores'),(2,'USER','Usuarios');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `rol_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_kfsp0s1tflm1cwlj8idhqsad0` (`email`),
+  KEY `FKqf5elo4jcq7qrt83oi0qmenjo` (`rol_id`),
+  CONSTRAINT `FKqf5elo4jcq7qrt83oi0qmenjo` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'javier@email.net','Javier','javier',1),(2,'pepe@email.net','Pepe','pepe',2);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'mf0226jpa'
 --
 
 --
--- Dumping routines for database 'mf0226'
+-- Dumping routines for database 'mf0226jpa'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -60,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-08 13:53:45
+-- Dump completed on 2023-05-12  8:17:42
