d815ceda0c8ec93d69323aea978a35b82fc8d7bb
Javier Lete <javier@eskura21.com>
2023-05-18 09:49:25 +0200

    Dao genérico copiado

---
+++ MusicaMVC/src/main/java/com/ipartek/formacion/musicamvc/accesodatos/Dao.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.musicamvc.accesodatos;
+
+public interface Dao<T> {
+	Iterable<T> obtenerTodos();
+	T obtenerPorId(Long id);
+	
+	T insertar(T objeto);
+	T modificar(T objeto);
+	void borrar(Long id);
+}
