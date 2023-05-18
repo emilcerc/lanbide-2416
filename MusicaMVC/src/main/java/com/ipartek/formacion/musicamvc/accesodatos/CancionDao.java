ad8b9805a866a42fe321a982735a60ef0f373384
Javier Lete <javier@eskura21.com>
2023-05-18 11:06:22 +0200

    Retoque de DAO de canción para soportar operaciones nuevas

--- MusicaMVC/src/main/java/com/ipartek/formacion/musicamvc/accesodatos/CancionDao.java
+++ MusicaMVC/src/main/java/com/ipartek/formacion/musicamvc/accesodatos/CancionDao.java
@@ -9,4 +9,8 @@ public interface CancionDao extends Dao<Cancion> {
 	Iterable<Cancion> buscarPorNombreUsuario(String nombre);
 	Iterable<Cancion> buscarPorEmail(String email);
 	Iterable<Cancion> buscarPorIdUsuario(Long id);
+	
+	Iterable<Cancion> buscarPorNombreYUsuarioId(String nombre, Long idUsuario);
+	Iterable<Cancion> buscarPorEstiloYUsuarioId(String estilo, Long idUsuario);
+	Iterable<Cancion> buscarPorGrupoYUsuarioId(String grupo, Long idUsuario);
 }
