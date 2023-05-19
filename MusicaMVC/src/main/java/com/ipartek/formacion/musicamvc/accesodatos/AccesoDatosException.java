255edc1dd5b26e2e6b0192acfece54dcb66eb1cb
Javier Lete <javier@eskura21.com>
2023-05-19 11:19:41 +0200

    Nueva excepción de Acceso a datos

---
+++ MusicaMVC/src/main/java/com/ipartek/formacion/musicamvc/accesodatos/AccesoDatosException.java
@@ -0,0 +1,10 @@
+package com.ipartek.formacion.musicamvc.accesodatos;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class AccesoDatosException extends RuntimeException {
+
+	private static final long serialVersionUID = -1770275701776937992L;
+
+}
