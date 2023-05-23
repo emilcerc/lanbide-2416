4c11debed60703fa082735e9cef097cc8a862753
Javier Lete <javier@eskura21.com>
2023-05-23 10:06:25 +0200

    Validaciones

--- springmvc/src/main/java/com/ipartek/formacion/springmvc/presentacion/controladores/ProductoController.java
+++ springmvc/src/main/java/com/ipartek/formacion/springmvc/presentacion/controladores/ProductoController.java
@@ -1,24 +1,32 @@
 package com.ipartek.formacion.springmvc.presentacion.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ipartek.formacion.springmvc.presentacion.modelos.Producto;
 
+import jakarta.validation.Valid;
+
 @Controller
 @RequestMapping("/producto")
 public class ProductoController {
 	@GetMapping
-	public String mostrarFormulario() {
+	public String mostrarFormulario(Producto producto) {
 		return "producto";
 	}
 	
 	@PostMapping
-	public String recepcionFormulario(Producto producto) {
+	public String recepcionFormulario(@Valid Producto producto, BindingResult bindingResult) {
+		System.out.println(bindingResult);
 		System.out.println(producto);
 		
-		return null;
+		if(bindingResult.hasErrors()) {
+			return "producto";
+		}
+		
+		return "redirect:/";
 	}
 }
--- springmvc/src/main/java/com/ipartek/formacion/springmvc/presentacion/modelos/Producto.java
+++ springmvc/src/main/java/com/ipartek/formacion/springmvc/presentacion/modelos/Producto.java
@@ -3,12 +3,31 @@ package com.ipartek.formacion.springmvc.presentacion.modelos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Producto {
+	@Min(0)
 	private Long id;
+	
+	@NotNull
+	@NotBlank
+	@Size(max = 50)
 	private String nombre;
+	
+	@NotNull
+	@Min(0)
 	private BigDecimal precio;
+	
+	@DateTimeFormat(iso = ISO.DATE)
+	@FutureOrPresent
 	private LocalDate caducidad;
 }
