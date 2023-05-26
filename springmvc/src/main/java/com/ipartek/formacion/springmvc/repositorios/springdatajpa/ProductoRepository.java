fb9703cfc60d722df62184d8e16f3bfd7dd7c85c
Javier Lete <javier@eskura21.com>
2023-05-26 10:58:08 +0200

    Repositorio automático Spring Data Rest

--- springmvc/src/main/java/com/ipartek/formacion/springmvc/repositorios/springdatajpa/ProductoRepository.java
+++ springmvc/src/main/java/com/ipartek/formacion/springmvc/repositorios/springdatajpa/ProductoRepository.java
@@ -3,10 +3,12 @@ package com.ipartek.formacion.springmvc.repositorios.springdatajpa;
 import java.math.BigDecimal;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ipartek.formacion.springmvc.entidades.Producto;
 
+@RepositoryRestResource(collectionResourceRel = "productos", path="productos")
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
-	Producto findByNombreContains(String nombre);
+	Iterable<Producto> findByNombreContains(String nombre);
 	Iterable<Producto> findByPrecioBetween(BigDecimal min, BigDecimal max);
 }
