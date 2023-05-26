55f908d61fec48eff5051ed49afa081673809e27
Javier Lete <javier@eskura21.com>
2023-05-26 10:27:02 +0200

    RestController con estados

--- springmvc/src/main/java/com/ipartek/formacion/springmvc/rest/ProductoRestController.java
+++ springmvc/src/main/java/com/ipartek/formacion/springmvc/rest/ProductoRestController.java
@@ -1,6 +1,7 @@
 package com.ipartek.formacion.springmvc.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ipartek.formacion.springmvc.entidades.Producto;
 import com.ipartek.formacion.springmvc.servicios.ProductoService;
@@ -26,20 +29,31 @@ public class ProductoRestController {
 	
 	@GetMapping("/{id}")
 	public Producto getPorId(@PathVariable Long id) {
-		return servicio.detalle(id);
+		Producto producto = servicio.detalle(id);
+		
+		if(producto == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		
+		return producto;
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Producto post(@RequestBody Producto producto) {
 		return servicio.guardar(producto);
 	}
 	
 	@PutMapping("/{id}")
 	public Producto put(@PathVariable Long id, @RequestBody Producto producto) {
+		if(id != producto.getId()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 		return servicio.modificar(producto);
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) {
 		servicio.borrar(id);
 	}
