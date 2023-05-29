c3e40d243336720c8a4be7b6a174a33f6771a551
Javier Lete <javier@eskura21.com>
2023-05-29 08:29:00 +0200

    Inicio proyecto MusicaMVCSpring

---
+++ musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/MusicamvcspringApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.ipartek.musicamvcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicamvcspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicamvcspringApplication.class, args);
+	}
+
+}
---
+++ musicamvcspring/src/test/java/com/formacion/ipartek/musicamvcspring/MusicamvcspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.ipartek.musicamvcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicamvcspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
