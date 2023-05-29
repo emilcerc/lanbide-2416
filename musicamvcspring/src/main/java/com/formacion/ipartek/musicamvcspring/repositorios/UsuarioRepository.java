87ccd2500aad5d07d7d2b41cbb2f0bae49cf4141
Javier Lete <javier@eskura21.com>
2023-05-29 11:08:15 +0200

    Búsquedas de usuario

--- musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/repositorios/UsuarioRepository.java
+++ musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/repositorios/UsuarioRepository.java
@@ -1,11 +1,25 @@
 package com.formacion.ipartek.musicamvcspring.repositorios;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.formacion.ipartek.musicamvcspring.entidades.Cancion;
 import com.formacion.ipartek.musicamvcspring.entidades.Usuario;
 
 @RepositoryRestResource(path = "usuarios", collectionResourceRel = "usuarios")
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 	Usuario findByEmail(String email);
+	
+	@Query("from Cancion c join c.usuario u where u.id = ?1")
+	Iterable<Cancion> buscarPorId(Long id);
+	
+	@Query("from Cancion c join c.usuario u where u.id = ?1 and c.titulo like %?2%")
+	Iterable<Cancion> buscarPorNombre(Long id, String nombre);
+	
+	@Query("from Cancion c join c.usuario u join c.estilo e where u.id = ?1 and e.nombre like %?2%")
+	Iterable<Cancion> buscarPorEstilo(Long id, String estilo);
+
+	@Query("from Cancion c join c.usuario u join c.grupo g where u.id = ?1 and g.nombre like %?2%")
+	Iterable<Cancion> buscarPorGrupo(Long id, String grupo);
 }
