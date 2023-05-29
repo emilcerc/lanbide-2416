4b706d2c5d3e4321bbc2af102b33b271cc37fdfd
Javier Lete <javier@eskura21.com>
2023-05-29 09:56:03 +0200

    Entidades

---
+++ musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/entidades/Cancion.java
@@ -0,0 +1,52 @@
+package com.formacion.ipartek.musicamvcspring.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//JPA
+@Entity
+@Table(name = "canciones")
+public class Cancion {
+	// JPA
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// VALIDATION
+	@NotNull
+	@NotBlank
+	@Size(max = 50)
+	private String titulo;
+
+	// VALIDATION
+	@NotNull
+	@NotBlank
+	@Size(max = 255)
+	private String url;
+
+	// JPA
+	@NotNull
+	@ManyToOne
+	private Grupo grupo;
+	
+	// JPA
+	@NotNull
+	@ManyToOne
+	private Estilo estilo;
+}
---
+++ musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/entidades/Grupo.java
@@ -0,0 +1,47 @@
+package com.formacion.ipartek.musicamvcspring.entidades;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//JPA
+@Entity
+@Table(name = "grupos")
+public class Grupo {
+	// JPA
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// VALIDATION
+	@NotNull
+	@NotBlank
+	@Size(max = 100)
+	private String nombre;
+	
+	// JPA
+	@Lob
+	private String comentarios;
+	
+	// JPA
+	@OneToMany(mappedBy = "grupo")
+	private Set<Cancion> canciones;
+}
