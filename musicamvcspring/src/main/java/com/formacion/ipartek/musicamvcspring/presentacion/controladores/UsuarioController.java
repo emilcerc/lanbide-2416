a9e95fe3464944d480a4664c0a90fc92994580d4
Javier Lete <javier@eskura21.com>
2023-05-30 14:12:04 +0200

    Utilizamos el servicio de usuario para pedir las canciones

--- musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/presentacion/controladores/UsuarioController.java
+++ musicamvcspring/src/main/java/com/formacion/ipartek/musicamvcspring/presentacion/controladores/UsuarioController.java
@@ -1,14 +1,22 @@
 package com.formacion.ipartek.musicamvcspring.presentacion.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.formacion.ipartek.musicamvcspring.servicios.UsuarioService;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	@GetMapping
-	public String index() {
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping({"", "canciones"})
+	public String index(Model modelo) {
+		modelo.addAttribute("canciones", usuarioService.obtenerSusCanciones());
 		return "usuarios/index";
 	}
 }
