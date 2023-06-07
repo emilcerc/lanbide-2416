b54359a22967e3a472ebcf63577ecf568ae95b4b
Javier Lete <javier@eskura21.com>
2023-06-07 09:46:46 +0200

    Curso con identificador único

--- gestionformacion/src/main/java/com/ipartek/formacion/gestionformacion/entidades/Curso.java
+++ gestionformacion/src/main/java/com/ipartek/formacion/gestionformacion/entidades/Curso.java
@@ -2,6 +2,7 @@ package com.ipartek.formacion.gestionformacion.entidades;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,6 +33,7 @@ public class Curso {
 	@NotBlank
 	@Pattern(regexp = "[A-Z]{4}\\d{4}")
 	@Size(min = 8, max = 8)
+	@Column(unique = true)
 	private String identificador;
 	
 	@NotNull
