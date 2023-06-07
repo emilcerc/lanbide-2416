5a07e3109d13d6432b49e40b3df3b2592174d7e9
Javier Lete <javier@eskura21.com>
2023-06-07 08:26:13 +0200

    Inicio proyecto gestionformacion

---
+++ gestionformacion/src/main/java/com/ipartek/formacion/gestionformacion/GestionformacionApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.gestionformacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionformacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionformacionApplication.class, args);
+	}
+
+}
---
+++ gestionformacion/src/main/java/com/ipartek/formacion/gestionformacion/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.gestionformacion;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestionformacionApplication.class);
+	}
+
+}
