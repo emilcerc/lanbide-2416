77ad876d93ce106974db90b9242edab00772f6a5
Javier Lete <javier@eskura21.com>
2023-06-07 09:47:10 +0200

    Listado de cursos (pero con muchas SELECT)

---
+++ gestionformacion/src/main/java/com/ipartek/formacion/gestionformacion/presentacion/controladores/CursoController.java
@@ -0,0 +1,22 @@
+package com.ipartek.formacion.gestionformacion.presentacion.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ipartek.formacion.gestionformacion.servicios.CursoService;
+
+@Controller
+@RequestMapping("/")
+public class CursoController {
+	@Autowired
+	private CursoService servicio;
+	
+	@GetMapping
+	public String listado(Model modelo) {
+		modelo.addAttribute("cursos", servicio.listado());
+		return "listado";
+	}
+}
